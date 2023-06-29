Fix scaling bug

spotipy/model.py
@@ -686,7 +686,7 @@ class SpotNet(CARE):
             return self.keras_model.predict(x[np.newaxis], **kwargs)[0,...,0]
 
     
-    def predict(self, img, prob_thresh=None, n_tiles=(1,1), subpix = False, min_distance=2, scale = None, 
+    def predict(self, img, prob_thresh=None, n_tiles=(1,1), subpix = False, min_distance=1, scale = None, 
                     normalizer=None, return_details:bool = False, peak_mode:str='skimage', exclude_border:bool=True, 
                     verbose:bool=True, show_tile_progress:bool=False):
         if img.ndim==2:
@@ -707,7 +707,8 @@ class SpotNet(CARE):
         div_by = self._axes_div_by("YXC")
         pad_shape = tuple(int(d*np.ceil(s/d)) for s,d in zip(x.shape, div_by))
         if verbose: print(f"Padding to shape {pad_shape}")
-        x = center_pad(x, pad_shape, mode="reflect")
+        x, padding = center_pad(x, pad_shape, mode="reflect")
+        
         if verbose: print("Predicting...")
         
         prob_kwargs = dict(prob_thresh=prob_thresh, subpix=subpix, min_distance=min_distance, mode=peak_mode, exclude_border=exclude_border)
@@ -747,11 +748,10 @@ class SpotNet(CARE):
                     tile = normalizer(tile)
                 y_tile = self._predict_prob(tile, verbose=False)
 
-
                 # points that include padded region 
                 p = prob_to_points(y_tile, **prob_kwargs)
                 
-                # remove offset 
+                # remove global offset 
                 p -= np.array([s.start for s in s_src[:2]])[None]
                 write_shape = tuple(s.stop-s.start for s in s_dst[:2])
                 # filter points that are outside of the write tile region 
@@ -778,7 +778,14 @@ class SpotNet(CARE):
 
             points = np.concatenate(points, axis=0)
 
+            # remove padding 
+            points = points - np.array((padding[0][0], padding[1][0]))[None]
+
             probs = np.array(probs)
+            if scale is not None:
+                points = points/scale
+                
+                
             probs = _filter_shape(probs, img.shape[:2], idxr_array=points)
             points = _filter_shape(points, img.shape[:2], idxr_array=points)
 

spotipy/utils.py
@@ -370,7 +370,8 @@ def center_pad(x, shape, mode = "reflect"):
     if not all([s1<=s2 for s1,s2 in zip(x.shape,shape)]):
         raise ValueError(f"shape of x {x.shape} is larger than final shape {shape}")
     diff = np.array(shape)- np.array(x.shape)
-    return np.pad(x,tuple((int(np.ceil(d/2)),d-int(np.ceil(d/2))) if d>0 else (0,0) for d in diff), mode=mode)
+    pads = tuple((int(np.ceil(d/2)),d-int(np.ceil(d/2))) if d>0 else (0,0) for d in diff)
+    return np.pad(x,pads, mode=mode), pads
 
 
 def center_crop(x, shape):

tests/test_model.py
@@ -1,21 +1,21 @@
 import numpy as np 
 from spotipy.model import Config, SpotNetData, SpotNet
 from spotipy.utils import points_to_prob
+from spotipy.utils import normalize
 
+np.random.seed(42) 
 
 def dummy_data(n_samples=16):
-    X = np.random.uniform(0,1,(n_samples, 128, 128))
-    P = np.random.randint(0,128,(n_samples, 21, 2))
-    for x, p in zip(X, P):
-        x[tuple(p.T.tolist())] = np.random.uniform(2,5,len(p))
-    Y = np.stack(tuple(points_to_prob(p, (128,128)) for p in P))
+    
+    P = np.random.randint(0,256,(n_samples, 21, 2))
+    X = np.stack(tuple(points_to_prob(p, (256,256), sigma=1.5) for p in P)) 
+    Y = np.stack(tuple(points_to_prob(p, (256,256), sigma=1.5) for p in P))
+    X = X + np.random.normal(0,0.1, X.shape)
     return X, Y, P
 
 
+def test_train():
 
-if __name__ == '__main__':
-
-    
     config = Config(n_channel_in=1, train_patch_size=(64,64), backbone='unet', spot_sigma=1)
 
     model = SpotNet(config, name = None, basedir = None)
@@ -30,4 +30,13 @@ if __name__ == '__main__':
     points, _ = model.predict(X[0])
     points2, _ = model.predict(X[0], peak_mode='fast')
 
+if __name__ == '__main__':
+
+    X,Y, P = dummy_data(128)
     
+    
+    model = SpotNet.from_pretrained('hybiss')
+    
+    
+    p1, d1 = model.predict(X[0], scale=.73, return_details=True)
+    p2, d2 = model.predict(X[0], scale=0.73, n_tiles=(2,2), return_details=True)